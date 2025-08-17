d24282f21f2db66b3c60a6c806dec5179d9a9ff1
iwillnotdoitlol <kobechalkley2@gmail.com>
2025-08-17 20:42:48 +0100

    Add files via upload

---
+++ Ro-Doxx.bat
@@ -0,0 +1,27 @@
+@echo off
+chcp 65001 >nul
+title ASCII Art
+
+:: Show ASCII art and menu once at the top
+echo                                   ▄▄▄        ·▄▄▄▄        ▐▄• ▄ ▐▄• ▄
+echo                                   ▀▄ █·▪     ██▪ ██ ▪      █▌█▌▪ █▌█▌▪
+echo                                   ▐▀▀▄  ▄█▀▄ ▐█· ▐█▌ ▄█▀▄  ·██·  ·██·
+echo                                   ▐█•█▌▐█▌.▐▌██. ██ ▐█▌.▐▌▪▐█·█▌▪▐█·█▌
+echo                                   .▀  ▀ ▀█▄▀▪▀▀▀▀▀•  ▀█▄▀▪•▀▀ ▀▀•▀▀ ▀▀
+echo                                   =====================================
+echo                                   01 - View RoDoxx Page
+echo.
+
+:loop
+set /p option="> "
+
+if "%option%"=="01" goto rodoxx
+if "%option%"=="1" goto rodoxx
+if /i "%option%"=="View RoDoxx Page" goto rodoxx
+
+goto loop
+
+:rodoxx
+echo User // IncognitoTeleporter // Katy, Texas, United States.
+echo User // 2fine4u667 // Albertina, Western Cape, South Africa.
+goto loop
